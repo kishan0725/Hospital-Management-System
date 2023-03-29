7f3d9e7a04b2e9af45f5ed1b889cebde3bf894b2
Adivardhan Maheshwari <48666834+adivardhan1000@users.noreply.github.com>
2023-03-29 19:53:14 +0100

    fix password leakage

--- admin-panel1.php
+++ admin-panel1.php
@@ -266,7 +266,6 @@ if(isset($_POST['docsub1']))
                     <th scope="col">Doctor Name</th>
                     <th scope="col">Specialization</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Password</th>
                     <th scope="col">Fees</th>
                   </tr>
                 </thead>
@@ -280,14 +279,12 @@ if(isset($_POST['docsub1']))
                       $username = $row['username'];
                       $spec = $row['spec'];
                       $email = $row['email'];
-                      $password = $row['password'];
                       $docFees = $row['docFees'];
                       
                       echo "<tr>
                         <td>$username</td>
                         <td>$spec</td>
                         <td>$email</td>
-                        <td>$password</td>
                         <td>$docFees</td>
                       </tr>";
                     }
@@ -318,7 +315,6 @@ if(isset($_POST['docsub1']))
                     <th scope="col">Gender</th>
                     <th scope="col">Email</th>
                     <th scope="col">Contact</th>
-                    <th scope="col">Password</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -334,7 +330,6 @@ if(isset($_POST['docsub1']))
                       $gender = $row['gender'];
                       $email = $row['email'];
                       $contact = $row['contact'];
-                      $password = $row['password'];
                       
                       echo "<tr>
                         <td>$pid</td>
@@ -343,7 +338,6 @@ if(isset($_POST['docsub1']))
                         <td>$gender</td>
                         <td>$email</td>
                         <td>$contact</td>
-                        <td>$password</td>
                       </tr>";
                     }
 
