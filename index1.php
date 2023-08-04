39edfbde31fa5794cde1c39b119c52e3bebcdda0
AmanSisodiya14 <129030306+AmanSisodiya14@users.noreply.github.com>
2023-08-04 17:49:05 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -108,7 +108,7 @@ include("header.php");
 
 
 
-
+//edit
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
