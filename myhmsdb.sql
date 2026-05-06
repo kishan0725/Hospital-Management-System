c5f992ce510b533dd4d2e906a408ecd8ed45fe5c
JosiasF29 <145461673+JosiasF29@users.noreply.github.com>
2026-05-06 08:11:54 +0300

    increase password columns to varchar(255) to support bcrypt hashes in tests

--- myhmsdb.sql
+++ myhmsdb.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admintb` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -116,7 +116,7 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 
 CREATE TABLE `doctb` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `spec` varchar(50) NOT NULL,
   `docFees` int(10) NOT NULL
@@ -149,8 +149,8 @@ CREATE TABLE `patreg` (
   `gender` varchar(10) NOT NULL,
   `email` varchar(30) NOT NULL,
   `contact` varchar(10) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `cpassword` varchar(30) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `cpassword` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
